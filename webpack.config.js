f6b5bba035dda6b10683c657ceb61eb0338cac93
inokawa <48897392+inokawa@users.noreply.github.com>
2020-10-01 21:50:07 +0900

    Add configs

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require("path");
+
+module.exports = {
+  mode: "production",
+  entry: "./src",
+  output: {
+    path: path.join(__dirname, "build"),
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: {
+          loader: "ts-loader",
+          options: {
+            transpileOnly: true,
+          },
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".ts", ".js", ".json"],
+  },
+};
