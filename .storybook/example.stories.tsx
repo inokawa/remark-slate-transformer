e34525506fb9a1f27d53bd4ed25f5bf311b085c9
inokawa <48897392+inokawa@users.noreply.github.com>
2020-10-08 13:42:01 +0900

    Fix story

--- .storybook/example.stories.tsx
+++ .storybook/example.stories.tsx
@@ -1,17 +1,20 @@
-import React from "react";
+import unified from "unified";
+import markdown from "remark-parse";
+import { remarkToSlate } from "../src";
 import Editor from "./editor";
+import MdStyle from "./md-style";
+import text from "../fixture/article.md";
+
+const processor = unified()
+  .use(markdown, { commonmark: true })
+  .use(remarkToSlate as any);
 
 export default {
   title: "example",
 };
 
 export const Sample = () => (
-  <Editor
-    initialValue={[
-      {
-        type: "paragraph",
-        children: [{ text: "A line of text in a paragraph." }],
-      },
-    ]}
-  />
+  <MdStyle>
+    <Editor initialValue={processor.processSync(text).result as any} />
+  </MdStyle>
 );
