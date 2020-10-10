fdef013d656548ec0b834e972ef7194262f8dc0c
inokawa <48897392+inokawa@users.noreply.github.com>
2020-10-10 18:02:28 +0900

    Add decoration fixture

---
+++ fixture/decoration.md
@@ -0,0 +1,12 @@
+aaa **aaaaaaa** aaaaaa _aaaaaaaa_ aaaaaaaa ~~aaaaaaa~~ aaaa `aaaaaaaa` aaa
+aaaaaaaaaa _aa **a ~~aa~~ aa `aaa` aaaaa** aaaa_ aaaaaa
+
+# **heading**
+
+- **list**
+
+> **quote**
+
+**[link to GitHub.com](https://github.com/)**
+
+**![image](https://github.githubassets.com/images/modules/logos_page/Octocat.png "image")**
--- src/index.spec.js
+++ src/index.spec.js
@@ -5,25 +5,31 @@ import markdown from "remark-parse";
 import stringify from "remark-stringify";
 import { remarkToSlate, slateToRemark } from "./";
 
+const FIXTURE_PATH = "../fixture";
+
 describe("e2e", () => {
   const toSlateProcessor = unified()
     .use(markdown, { commonmark: true })
     .use(remarkToSlate);
   const toRemarkProcessor = unified().use(slateToRemark).use(stringify);
 
-  it.each([["article.md"], ["headings.md"], ["plain-text.md"]])("%s", (p) => {
-    const slateNodes = toSlateProcessor.processSync(
-      fs.readFileSync(path.join(__dirname, "../fixture/", p))
-    ).result;
-    expect(slateNodes).toMatchSnapshot();
+  const fixtureDir = path.join(__dirname, FIXTURE_PATH);
+  const filenames = fs.readdirSync(fixtureDir);
+  filenames.forEach((filename) => {
+    it(filename, () => {
+      const slateNodes = toSlateProcessor.processSync(
+        fs.readFileSync(path.join(fixtureDir, filename))
+      ).result;
+      expect(slateNodes).toMatchSnapshot();
 
-    const mdastTree = toRemarkProcessor.runSync({
-      type: "root",
-      children: slateNodes,
+      const mdastTree = toRemarkProcessor.runSync({
+        type: "root",
+        children: slateNodes,
+      });
+      expect(mdastTree).toMatchSnapshot();
+
+      const text = toRemarkProcessor.stringify(mdastTree);
+      expect(text).toMatchSnapshot();
     });
-    expect(mdastTree).toMatchSnapshot();
-
-    const text = toRemarkProcessor.stringify(mdastTree);
-    expect(text).toMatchSnapshot();
   });
 });
