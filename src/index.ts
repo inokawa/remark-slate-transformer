a7b35d346ef57fac71f06126135a2edb0ac7cde3
David Golden <34049153+davidgolden@users.noreply.github.com>
2021-06-30 18:21:55 -0600

    export mdastToSlate/slateToMdast transformers (#58)
    Co-authored-by: David Golden <david@soprisapps.com>

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,5 @@ export { default as remarkToSlate } from "./plugins/remark-to-slate";
 export { default as slateToRemark } from "./plugins/slate-to-remark";
 export { default as slateToRemarkLegacy } from "./plugins/slate0.47-to-remark";
 export { default as remarkToSlateLegacy } from "./plugins/remark-to-slate0.47";
+export { mdastToSlate } from "./transformers/mdast-to-slate";
+export { slateToMdast } from "./transformers/slate-to-mdast";
