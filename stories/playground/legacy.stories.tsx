03966f23a997f7e1848b05a1b79b80924a58b9d6
inokawa <48897392+inokawa@users.noreply.github.com>
2022-09-10 17:36:26 +0900

    Migrate to CSF3

--- stories/playground/legacy.stories.tsx
+++ stories/playground/legacy.stories.tsx
@@ -1,3 +1,4 @@
+import { StoryObj } from "@storybook/react";
 import React, { useMemo, useRef, useState } from "react";
 import { Value } from "slate_legacy";
 import { unified } from "unified";
@@ -5,11 +6,11 @@ import markdown from "remark-parse";
 import gfm from "remark-gfm";
 import frontmatter from "remark-frontmatter";
 import stringify from "remark-stringify";
-import { remarkToSlateLegacy, slateToRemarkLegacy } from "../src";
-import SlateEditor from "./components/slate0.47-editor";
-import TextEditor from "./components/text-editor";
-import Text from "./components/text";
-import text from "../fixtures/article.md";
+import { remarkToSlateLegacy, slateToRemarkLegacy } from "../../src";
+import SlateEditor from "../components/slate0.47-editor";
+import TextEditor from "../components/text-editor";
+import Text from "../components/text";
+import text from "../../fixtures/article.md";
 
 const toSlateProcessor = unified()
   .use(markdown)
@@ -75,47 +76,51 @@ const Wrapper = ({ children }: { children: React.ReactNode }) => (
 //   );
 // };
 
-export const MarkdownToSlateJson = () => {
-  const [value, setValue] = useState(toSlate(text));
-  const ref = useRef<HTMLTextAreaElement>(null);
-  return (
-    <Wrapper>
-      <TextEditor ref={ref} initialValue={text} />
-      <div style={{ padding: 10 }}>
-        <button
-          style={{ height: "100%" }}
-          onClick={() => {
-            if (!ref.current) return;
-            setValue(toSlate(ref.current.value));
-          }}
-        >
-          {"md -> slate"}
-        </button>
-      </div>
-      <Text>{JSON.stringify(value, null, 2)}</Text>
-    </Wrapper>
-  );
+export const MarkdownToSlateJson: StoryObj = {
+  render: () => {
+    const [value, setValue] = useState(toSlate(text));
+    const ref = useRef<HTMLTextAreaElement>(null);
+    return (
+      <Wrapper>
+        <TextEditor ref={ref} initialValue={text} />
+        <div style={{ padding: 10 }}>
+          <button
+            style={{ height: "100%" }}
+            onClick={() => {
+              if (!ref.current) return;
+              setValue(toSlate(ref.current.value));
+            }}
+          >
+            {"md -> slate"}
+          </button>
+        </div>
+        <Text>{JSON.stringify(value, null, 2)}</Text>
+      </Wrapper>
+    );
+  },
 };
 
-// export const SlateToMarkdown = () => {
-//   const [value, setValue] = useState(toSlate(text));
-//   const [md, setMd] = useState(toMd(value));
-//   const ref = useRef<Value>(null);
-//   return (
-//     <Wrapper>
-//       <SlateEditor ref={ref} initialValue={value} />
-//       <div style={{ padding: 10 }}>
-//         <button
-//           style={{ height: "100%" }}
-//           onClick={() => {
-//             if (!ref.current) return;
-//             setMd(toMd(ref.current));
-//           }}
-//         >
-//           {"slate -> md"}
-//         </button>
-//       </div>
-//       <Text>{md}</Text>
-//     </Wrapper>
-//   );
+// export const SlateToMarkdown: StoryObj = {
+//   render: () => {
+//     const [value, setValue] = useState(toSlate(text));
+//     const [md, setMd] = useState(toMd(value));
+//     const ref = useRef<Value>(null);
+//     return (
+//       <Wrapper>
+//         <SlateEditor ref={ref} initialValue={value} />
+//         <div style={{ padding: 10 }}>
+//           <button
+//             style={{ height: "100%" }}
+//             onClick={() => {
+//               if (!ref.current) return;
+//               setMd(toMd(ref.current));
+//             }}
+//           >
+//             {"slate -> md"}
+//           </button>
+//         </div>
+//         <Text>{md}</Text>
+//       </Wrapper>
+//     );
+//   },
 // };
