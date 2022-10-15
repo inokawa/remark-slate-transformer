e74ab36f3b5314690ffd263136e817578cf0e243
xiaolu-lujunji <xiaolu.lujunji@gmail.com>
2022-10-15 21:09:47 +0800

    Type error about extending mdast

--- src/models/mdast.ts
+++ src/models/mdast.ts
@@ -1,75 +1,8 @@
 // ref: https://github.com/syntax-tree/mdast
 
-import type {
-  Parent,
-  Literal,
-  Root,
-  Paragraph,
-  Heading,
-  ThematicBreak,
-  Blockquote,
-  List,
-  ListItem,
-  Table,
-  TableRow,
-  TableCell,
-  HTML,
-  Code,
-  YAML,
-  Definition,
-  FootnoteDefinition,
-  Text,
-  Emphasis,
-  Strong,
-  Delete,
-  InlineCode,
-  Break,
-  Link,
-  Image,
-  LinkReference,
-  ImageReference,
-  Footnote,
-  FootnoteReference,
-  Resource,
-  Association,
-  Reference,
-  Alternative,
-} from "mdast";
-export type {
-  Parent,
-  Literal,
-  Root,
-  Paragraph,
-  Heading,
-  ThematicBreak,
-  Blockquote,
-  List,
-  ListItem,
-  Table,
-  TableRow,
-  TableCell,
-  HTML,
-  Code,
-  YAML,
-  Definition,
-  FootnoteDefinition,
-  Text,
-  Emphasis,
-  Strong,
-  Delete,
-  InlineCode,
-  Break,
-  Link,
-  Image,
-  LinkReference,
-  ImageReference,
-  Footnote,
-  FootnoteReference,
-  Resource,
-  Association,
-  Reference,
-  Alternative,
-};
+import type { Literal } from 'mdast'
+
+export * from 'mdast'
 
 export interface TOML extends Literal {
   type: "toml";
@@ -83,51 +16,16 @@ export interface InlineMath extends Literal {
   type: "inlineMath";
 }
 
-export type Content =
-  | TopLevelContent
-  | ListContent
-  | TableContent
-  | RowContent
-  | PhrasingContent;
+declare module "mdast" {
+  interface FrontmatterContentMap {
+    toml: TOML
+  }
 
-export type TopLevelContent =
-  | BlockContent
-  | FrontmatterContent
-  | DefinitionContent;
+  interface BlockContentMap {
+    math: Math
+  }
 
-export type BlockContent =
-  | Paragraph
-  | Heading
-  | ThematicBreak
-  | Blockquote
-  | List
-  | Table
-  | HTML
-  | Code
-  | Math;
-
-export type FrontmatterContent = YAML | TOML;
-
-export type DefinitionContent = Definition | FootnoteDefinition;
-
-export type ListContent = ListItem;
-
-export type TableContent = TableRow;
-
-export type RowContent = TableCell;
-
-export type PhrasingContent = StaticPhrasingContent | Link | LinkReference;
-
-export type StaticPhrasingContent =
-  | Text
-  | Emphasis
-  | Strong
-  | Delete
-  | HTML
-  | InlineCode
-  | Break
-  | Image
-  | ImageReference
-  | Footnote
-  | FootnoteReference
-  | InlineMath;
+  interface StaticPhrasingContentMap {
+    inlineMath: InlineMath
+  }
+}
