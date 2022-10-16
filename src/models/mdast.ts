e7ec86724c409aa4aede50618558efb4b6881340
lujunji <xiaolu.lujunji@gmail.com>
2022-10-16 08:52:01 +0800

    Update mdast.ts
    Fix: Subsequent property declarations must have the same type.  Property 'math' must be of type 'Math', but here has type 'Math'.

--- src/models/mdast.ts
+++ src/models/mdast.ts
@@ -3,29 +3,14 @@
 import type { Literal } from 'mdast'
 
 export * from 'mdast'
+export * from 'mdast-util-math/complex-types'
 
 export interface TOML extends Literal {
   type: "toml";
 }
 
-export interface Math extends Literal {
-  type: "math";
-}
-
-export interface InlineMath extends Literal {
-  type: "inlineMath";
-}
-
 declare module "mdast" {
   interface FrontmatterContentMap {
     toml: TOML
-  }
-
-  interface BlockContentMap {
-    math: Math
-  }
-
-  interface StaticPhrasingContentMap {
-    inlineMath: InlineMath
   }
 }
