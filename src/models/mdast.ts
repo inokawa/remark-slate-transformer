44476ec9bd39e8e754b7547deb1ae9f012cf325c
inokawa <48897392+inokawa@users.noreply.github.com>
2022-10-16 21:43:52 +0900

    0.7.2

--- src/models/mdast.ts
+++ src/models/mdast.ts
@@ -1,9 +1,9 @@
 // ref: https://github.com/syntax-tree/mdast
 
-import type { Literal } from 'mdast'
+import type { Literal } from "mdast";
 
-export * from 'mdast'
-export * from 'mdast-util-math/complex-types'
+export * from "mdast";
+export * from "mdast-util-math/complex-types";
 
 export interface TOML extends Literal {
   type: "toml";
@@ -11,6 +11,6 @@ export interface TOML extends Literal {
 
 declare module "mdast" {
   interface FrontmatterContentMap {
-    toml: TOML
+    toml: TOML;
   }
 }
