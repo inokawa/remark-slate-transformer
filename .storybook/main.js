e0bd6082a9a3fd0642a97753faecde7b50527104
inokawa <48897392+inokawa@users.noreply.github.com>
2022-11-30 03:07:06 +0900

    Upgrade to storybook v7 alpha

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,11 +3,27 @@ module.exports = {
     "../stories/**/*.stories.mdx",
     "../stories/**/*.stories.@(js|jsx|ts|tsx)",
   ],
-  addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
-  framework: "@storybook/react",
-  core: {
-    builder: {
-      name: "webpack5",
-    },
+  addons: ["@storybook/addon-docs"],
+  framework: {
+    name: "@storybook/react-webpack5",
+    options: {},
+  },
+  webpackFinal: async (config) => {
+    config.module.rules.push({
+      test: /\.tsx?$/,
+      use: [
+        {
+          loader: "ts-loader",
+          options: {
+            transpileOnly: true,
+          },
+        },
+      ],
+    });
+    config.resolve.fallback = {
+      // For micromark
+      assert: "assert",
+    };
+    return config;
   },
 };
