5486e19ab68d6f177c77d5b3bff8a9b088bb066c
inokawa <48897392+inokawa@users.noreply.github.com>
2023-08-11 00:30:43 +0900

    Use terser

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,4 +1,5 @@
 import typescript from "@rollup/plugin-typescript";
+import terser from "@rollup/plugin-terser";
 import pkg from "./package.json" assert { type: "json" };
 
 const externals = [
@@ -27,6 +28,18 @@ export default {
       outDir: ".",
       declaration: true,
       exclude: ["**/*.{spec,stories}.*"],
+    }),
+    terser({
+      ecma: 2015,
+      module: true,
+      compress: { passes: 5, unsafe: true, keep_fargs: false },
+      mangle: { properties: { regex: "^_" } },
+      format: {
+        // https://github.com/terser/terser/pull/550
+        // https://github.com/terser/terser/issues/968
+        comments: /@preserve|@lic|@cc_on|^\**!|__PURE__/i,
+        preserve_annotations: true,
+      },
     }),
   ],
 };
