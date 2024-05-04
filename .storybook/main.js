c5fbca2d1cfe663c4e95911a1ec7cc26571ccd38
inokawa <48897392+inokawa@users.noreply.github.com>
2024-05-04 21:47:34 +0900

    Update deps

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,10 +2,7 @@ import { mergeConfig } from "vite";
 
 /** @type { import('@storybook/react-vite').StorybookConfig } */
 export default {
-  stories: [
-    "../stories/**/*.stories.mdx",
-    "../stories/**/*.stories.@(js|jsx|ts|tsx)",
-  ],
+  stories: ["../stories/**/*.mdx", "../stories/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: ["@storybook/addon-storysource"],
   framework: {
     name: "@storybook/react-vite",
