947df82be141419b5acce19bdd93e11b57804702
inokawa <48897392+inokawa@users.noreply.github.com>
2024-05-12 21:16:01 +0900

    Update rollup.config.mjs

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,6 +1,6 @@
 import typescript from "@rollup/plugin-typescript";
 import terser from "@rollup/plugin-terser";
-import pkg from "./package.json" assert { type: "json" };
+import pkg from "./package.json" with { type: "json" };
 
 const externals = [
   ...Object.keys(pkg.dependencies),
